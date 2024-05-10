c80cac2f29190f643b5d7ad5f52e6553e0313984
MatsDer123 <148542178+MatsDer123@users.noreply.github.com>
2024-05-10 22:46:25 +0200

    Consumer mailing
    Description: Modify consuler for welcome mail
    Revieuwd by Mats

--- MailDynamic.py
+++ MailDynamic.py
@@ -13,13 +13,11 @@ FROM_EMAIL = 'mats.deraymaeker@student.ehb.be'
 # update to your dynamic template id from the UI
 TEMPLATE_ID = 'd-b3b117e3099a4b30baf380342afbd54f'
 
-# list of emails and preheader names, update with yours
-TO_EMAILS = [('mats.deraymaeker@gmail.com', 'James Holden')]
-             # update email and name
-             #('your_email+1@domain.com', 'Joe Miller')]
 
+def send_welcome_mail(email, name):
 
-def SendDynamic():
+    TO_EMAILS = [(email)]
+
     """ Send a dynamic email to a list of email addresses
 
     :returns API response code
@@ -30,21 +28,9 @@ def SendDynamic():
         to_emails=TO_EMAILS)
     # pass custom values for our HTML placeholders
     message.dynamic_template_data = {
-        'name': 'Tast Tester'
+        'name': name
     }
     message.template_id = TEMPLATE_ID
-    # create our sendgrid client object, pass it our key, then send and return our response objects
-    # try:
-    #     sg = SendGridAPIClient("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")
-    #     response = sg.send(message)
-    #     code, body, headers = response.status_code, response.body, response.headers
-    #     print(f"Response code: {code}")
-    #     print(f"Response headers: {headers}")
-    #     print(f"Response body: {body}")
-    #     print("Dynamic Messages Sent!")
-    # except Exception as e:
-    #     print("Error: {0}".format(e))
-    # return str(response.status_code)
 
     try:
         sg = SendGridAPIClient("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", ssl_context=context)
@@ -59,4 +45,4 @@ def SendDynamic():
 
 
 if __name__ == "__main__":
-    SendDynamic()
+    send_welcome_mail()
--- consumer_mailing.py
+++ consumer_mailing.py
@@ -1,76 +1,137 @@
 import pika
 from lxml import etree
 from io import BytesIO
+import MailDynamic
 
 # XSD schema definition
-xsd_string = '''
-<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
-    <xs:element name="Heartbeat">
+xsd_string = {
+    'status': '''
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+        <xs:element name="Heartbeat">
+            <xs:complexType>
+                <xs:sequence>
+                    <xs:element name="Timestamp" type="xs:dateTime" />
+                    <xs:element name="Status" type="xs:string" />
+                    <xs:element name="SystemName" type="xs:string" />
+                </xs:sequence>
+            </xs:complexType>
+        </xs:element>
+    </xs:schema>
+    ''',
+    'user': '''
+    <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+
+    <xs:element name="user">
         <xs:complexType>
             <xs:sequence>
-                <xs:element name="Timestamp" type="xs:dateTime" />
-                <xs:element name="Status" type="xs:string" />
-                <xs:element name="SystemName" type="xs:string" />
+                <xs:element name="routing_key" type="xs:string"/>
+                <xs:element name="user_id" type="xs:string"/>
+                <xs:element name="first_name" type="xs:string"/>
+                <xs:element name="last_name" type="xs:string"/>
+                <xs:element name="email" type="xs:string"/>
+                <xs:element name="telephone" type="xs:string"/>
+                <xs:element name="birthday" type="xs:date"/>
+                <xs:element name="address">
+                    <xs:complexType>
+                        <xs:sequence>
+                            <xs:element name="country" type="xs:string"/>
+                            <xs:element name="state" type="xs:string"/>
+                            <xs:element name="city" type="xs:string"/>
+                            <xs:element name="zip" type="xs:string"/>
+                            <xs:element name="street" type="xs:string"/>
+                            <xs:element name="house_number" type="xs:string"/>
+                        </xs:sequence>
+                    </xs:complexType>
+                </xs:element>
+                <xs:element name="company_email" type="xs:string" minOccurs="0"/>
+                <xs:element name="company_id" type="xs:string" minOccurs="0"/>
+                <xs:element name="source" type="xs:string" minOccurs="0"/>
+                <xs:element name="user_role">
+                    <xs:simpleType>
+                        <xs:restriction base="xs:string">
+                            <xs:enumeration value="Speaker"/>
+                            <xs:enumeration value="Individual"/>
+                            <xs:enumeration value="Employee"/>
+                        </xs:restriction>
+                    </xs:simpleType>
+                </xs:element>
+                <xs:element name="invoice">
+                    <xs:simpleType>
+                        <xs:restriction base="xs:string">
+                            <xs:enumeration value="Yes"/>
+                        </xs:restriction>
+                    </xs:simpleType>
+                </xs:element>
+                <xs:element name="calendar_link" />
             </xs:sequence>
         </xs:complexType>
     </xs:element>
-</xs:schema>
-'''
 
-def validate_xml(xml_string):
-    # Parse XSD schema
-    xsd_doc = etree.parse(BytesIO(xsd_string.encode('utf-8')))
-    xsd = etree.XMLSchema(xsd_doc)
+</xs:schema>''',
 
-    # Parse XML input
-    xml_doc = etree.parse(BytesIO(xml_string.encode('utf-8')))
+}
 
-    # Validate XML against XSD schema
+# Function to validate XML against embedded XSD schema
+def validate_xml(xml_str):
     try:
-        xsd.assertValid(xml_doc)
-        return True
-    except etree.DocumentInvalid as e:
-        print("Validation error:", e)
-        return False
-
-def process_message(ch, method, properties, body):
-    print("Received XML message:")
-    print(body.decode('utf-8'))
-
-    # Parse XML and extract Status
-    root = etree.fromstring(body)
-    status_element = root.find("Status")
-
-    if status_element is not None and status_element.text == "0":
-        # Validate the XML message
-        if validate_xml(body.decode('utf-8')):
-            # Process the valid XML
-            timestamp = root.find("Timestamp").text
-            status = root.find("Status").text
-            system_name = root.find("SystemName").text
-            
-            print("Timestamp:", timestamp)
-            print("Status:", status)
-            print("System Name:", system_name)
+        root = etree.fromstring(xml_str)
+        if root.tag in xsd_string:
+            xsd_str = xsd_string[root.tag]
+            xmlschema = etree.XMLSchema(etree.fromstring(xsd_str))
+            xmlparser = etree.XMLParser(schema=xmlschema)
+            etree.fromstring(xml_str, xmlparser)
+            return True, root  # Valid XML and root element
         else:
-            print("Invalid XML received. Discarding message.")
-    else:
-        print("Message does not contain 'Status' element with value '0'. Ignoring.")
+            return False, f"No schema available for the received XML root element '{root.tag}'"
+    except etree.XMLSchemaError as e:
+        return False, str(e)  # Invalid XML
 
-# RabbitMQ connection parameters
-rabbitmq_host = '10.2.160.51'
-queue_name = 'mailing'
+def send_welcome_mail(root_element):
+    try:
+        email = root_element.find('email').text
+        name = root_element.find('first_name').text
 
-# Establish connection to RabbitMQ
-credentials = pika.PlainCredentials('user', 'password')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host, credentials=credentials))
+        MailDynamic.send_welcome_mail(email, name)
+
+    except Exception as e:
+        print(f"Error sending welcome mail: {str(e)}")
+
+# Callback function for consuming messages
+def callback(ch, method, properties, body):
+    try:
+        xml_content = body.decode('utf-8')
+        is_valid, root_element = validate_xml(xml_content)
+        
+        if is_valid:
+            xml_type = root_element.tag
+            print(f"Received valid '{xml_type}' XML:")
+            print(xml_content)
+            
+            if xml_type == 'user':
+                send_welcome_mail(root_element)
+            elif xml_type == 'status':
+                send_welcome_mail(root_element)
+            elif xml_type == 'attendance':
+                send_welcome_mail(root_element)
+            else:
+                print(f"No handler defined for XML type: {xml_type}")
+        else:
+            print(f"Received invalid XML: {root_element}")
+    
+    except Exception as e:
+        print(f"Error processing message: {str(e)}")
+
+# Connect to RabbitMQ server
+credentials = pika.PlainCredentials('RABBITMQ_USER', 'RABBITMQ_PASSWORD')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='RABBITMQ_HOST', credentials=credentials))
 channel = connection.channel()
 
-# Declare the queue to consume from
-channel.queue_declare(queue=queue_name, durable=True)
+# Declare the queue
+channel.queue_declare(queue='mailing', durable=True)
 
-# Set up message consumption
-channel.basic_consume(queue=queue_name, on_message_callback=process_message, auto_ack=True)
+# Set up consumer to receive messages from the queue
+channel.basic_consume(queue='mailing', on_message_callback=callback, auto_ack=True)
 
-print(f"Waiting for messages with 'Status' = 'Down' from the queue '{queue_name}'...")
+# Start consuming messages
+print('Waiting for messages...')
 channel.start_consuming()
